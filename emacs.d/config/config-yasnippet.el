7539148e6f1f4942c1037242f257b97092538559
Sean Perry <shaleh@speakeasy.net>
2012-09-01 00:31:01 -0700

    fix incorrect snippet location

--- emacs.d/config/config-yasnippet.el
+++ emacs.d/config/config-yasnippet.el
@@ -1,7 +1,7 @@
 (require 'yasnippet)
 (setq yas-snippet-dirs
   (list (concat user-emacs-directory "my-snippets")
-        (concat user-emacs-directory "site-lisp/yasnippets/snippets")
+        (concat user-emacs-directory "site-lisp/yasnippet/snippets")
     )
  )
 (yas-global-mode 1)
