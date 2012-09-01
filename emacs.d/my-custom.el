5127d4a872a625dddd75794abf8396c8e2ade2bf
Sean Perry <shaleh@speakeasy.net>
2012-09-01 01:14:28 -0700

    add indicate empty lines

--- emacs.d/my-custom.el
+++ emacs.d/my-custom.el
@@ -4,6 +4,7 @@
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
  '(ido-mode (quote both) nil (ido))
+ '(indicate-empty-lines t)
  '(major-mode (quote text-mode))
  )
 (custom-set-faces
