0a1204851ce23663c1b598ff101dcc783db7219b
Sean Perry <shaleh@speakeasy.net>
2012-09-01 00:26:58 -0700

    add commands to handle trailing whitespace

--- emacs.d/my-lisp/functions.el
+++ emacs.d/my-lisp/functions.el
@@ -1,9 +1,21 @@
 (defun my/kill-other-buffers ()
   "Kill all other buffers."
   (interactive)
-  (mapc 'kill-buffer 
-    (delq (current-buffer) 
+  (mapc 'kill-buffer
+    (delq (current-buffer)
           (remove-if-not 'buffer-file-name (buffer-list))
      )
    )
+ )
+
+(defun my/hide-trailing-whitespace ()
+  "Do not highlight trailing whitespace in this buffer."
+  (interactive)
+  (setq show-trailing-whitespace nil)
+ )
+
+(defun my/show-trailing-whitespace ()
+  "Highlight trailing whitespace in this buffer."
+  (interactive)
+  (setq show-trailing-whitespace t)
  )
