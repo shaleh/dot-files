6dff5e6f496963c4771db4d78b021dd672f3b24f
Sean Perry <shaleh@speakeasy.net>
2012-09-05 12:18:02 -0700

    add my recent file function

--- emacs.d/my-lisp/functions.el
+++ emacs.d/my-lisp/functions.el
@@ -19,3 +19,31 @@
   (interactive)
   (setq show-trailing-whitespace t)
  )
+
+(defun my/recentf-ido-find-file ()
+  "Find a recent file using Ido."
+  (interactive)
+  (let* ((file-assoc-list
+	  (mapcar (lambda (x)
+		    (cons (file-name-nondirectory x)
+			  x)
+                   )
+		  recentf-list)
+          )
+	 (filename-list
+	  (remove-duplicates (mapcar #'car file-assoc-list)
+			     :test #'string=)
+          )
+	 (filename (ido-completing-read "Choose recent file: "
+					filename-list
+					nil
+					t)
+          )
+         )
+    (when filename
+      (find-file (cdr (assoc filename
+			     file-assoc-list))
+       )
+     )
+   )
+ )
