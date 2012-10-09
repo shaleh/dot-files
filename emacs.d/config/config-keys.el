707aa16cdcacfc2449f61be2bc3f9ba6894e6703
Sean Perry <shaleh@speakeasy.net>
2012-10-09 16:47:00 -0700

    add comment region binding

--- emacs.d/config/config-keys.el
+++ emacs.d/config/config-keys.el
@@ -11,3 +11,4 @@
 (global-set-key (kbd "M-y") 'kill-ring-ido) ; replace usual kill-ring-pop
 (global-set-key (kbd "<M-up>") 'my/move-line-up)
 (global-set-key (kbd "<M-down>") 'my/move-line-down)
+(global-set-key (kbd "C-c #") 'comment-region) ; use C-u C-c # to uncomment
