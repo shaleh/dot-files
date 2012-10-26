c097e99fc68a51f74afdbf3c619e3feed3cac6a2
Sean Perry <shaleh@speakeasy.net>
2012-10-26 14:08:38 -0700

    instead of advice, use minor mode.

--- emacs.d/config/config-modes-common.el
+++ emacs.d/config/config-modes-common.el
@@ -76,34 +76,35 @@
   (setq-default tab-width 4))
 
 
-;; Change cutting behavior:
-;; "Many times you'll do a kill-line command with the only intention of
-;; getting the contents of the line into the killring. Here's an idea stolen
-;; from Slickedit, if you press copy or cut when no region is active, you'll
-;; copy or cut the current line."
-;; <http://www.zafar.se/bkz/Articles/EmacsTips>
-(defadvice kill-ring-save (before slickcopy activate compile)
-  "When called interactively with no active region, copy the current line instead."
-  (interactive
-   (if mark-active
-       (list (region-beginning) (region-end))
-     (list (line-beginning-position)
-           (line-beginning-position 2))
-     )
-   )
-  )
+;; ;; Change cutting behavior:
+;; ;; "Many times you'll do a kill-line command with the only intention of
+;; ;; getting the contents of the line into the killring. Here's an idea stolen
+;; ;; from Slickedit, if you press copy or cut when no region is active, you'll
+;; ;; copy or cut the current line."
+;; ;; <http://www.zafar.se/bkz/Articles/EmacsTips>
+;; (defadvice kill-ring-save (before slickcopy activate)
+;;   "When called interactively with no active region, copy the current line instead."
+;;   (interactive
+;;    (if mark-active
+;;        (list (region-beginning) (region-end))
+;;      (list (line-beginning-position)
+;;            (line-beginning-position 2))
+;;      )
+;;    )
+;;   )
 
-(defadvice kill-region (before slickcut activate compile)
-  "When called interactively with no active region, kill the current line instead."
-  (interactive
-   (if mark-active
-       (list (region-beginning) (region-end))
-     (list (line-beginning-position)
-           (line-beginning-position 2))
-     )
-   )
-  )
+;; (defadvice kill-region (before slickcut activate)
+;;   "When called interactively with no active region, kill the current line instead."
+;;   (interactive
+;;    (if mark-active
+;;        (list (region-beginning) (region-end))
+;;      (list (line-beginning-position)
+;;            (line-beginning-position 2))
+;;      )
+;;    )
+;;   )
 
+(require 'whole-line-or-region)
 (defvar my/indented-modes '())
 
 ;; auto-indent pasted code
--- emacs.d/my-custom.el
+++ emacs.d/my-custom.el
@@ -1,14 +1,15 @@
 (custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(fringe-mode (quote (0)) nil (fringe))
  '(ido-mode (quote both) nil (ido))
- '(major-mode (quote text-mode)))
+ '(major-mode (quote text-mode))
+ '(whole-line-or-region-mode t))
 (custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  )
