8c51f2c81ad017d23a37cd8437bd535ded6d01ab
Sean Perry <shaleh@speakeasy.net>
2012-10-26 14:06:50 -0700

    add what-face

---
+++ emacs.d/site-lisp/what-face.el
@@ -0,0 +1,6 @@
+;; originally found here http://stackoverflow.com/questions/1242352/get-font-face-under-cursor-in-emacs
+(defun what-face (pos)
+  (interactive "d")
+  (let ((face (or (get-char-property (point) 'read-face-name)
+                  (get-char-property (point) 'face))))
+    (if face (message "Face: %s" face) (message "No face at %d" pos))))
