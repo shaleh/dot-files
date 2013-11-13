8c887f04a786ab68718196bb6928a5192868b3a3
Sean Perry <shaleh@speakeasy.net>
2013-11-12 17:11:03 -0800

    setup flymake output to be accessible from shell versions of emacs.

--- emacs.d/config/config-programming-common.el
+++ emacs.d/config/config-programming-common.el
@@ -1,6 +1,11 @@
 (autoload 'sperry-linum-mode "sperry-linum" "mode for line numbers" t)
 (setq-default sperry-linum-margin-padding 1)
 
+(custom-set-variables
+  '(help-at-pt-timer-delay 0.9)
+  '(help-at-pt-display-when-idle '(flymake-overlay))
+ )
+
 (defun my/common-programming-hook ()
    (turn-on-fic-mode)
    (flyspell-prog-mode)
