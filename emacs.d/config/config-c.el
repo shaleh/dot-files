c99be01061cabc6fa6f3f4ed5a1299dfc400919b
Sean Perry <shaleh@speakeasy.net>
2013-11-13 13:58:01 -0800

    c mode updates.

--- emacs.d/config/config-c.el
+++ emacs.d/config/config-c.el
@@ -1,1 +1,12 @@
-(add-hook 'c-mode-common-hook 'my/common-programming-hook)
+(add-hook 'c-mode-common-hook
+  (lambda()
+    (my/common-programming-hook)
+    (local-set-key (kbd "C-x C-o") 'ff-find-other-file)
+   )
+ )
+
+(add-hook 'c++-mode-hook
+    (lambda()
+        (c-set-style 'stroustrup)
+     )
+ )
