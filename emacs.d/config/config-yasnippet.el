e3d8a775bffec8eb73f6a09c648a797009548f7a
Sean Perry <shaleh@speakeasy.net>
2013-11-14 14:55:48 -0800

    fix yasnippet whitespace issues.

--- emacs.d/config/config-yasnippet.el
+++ emacs.d/config/config-yasnippet.el
@@ -7,3 +7,10 @@
  )
 
 (yas-global-mode 1)
+
+;; Occasionally there is incorrect whitespace left behind.
+(add-hook 'yas-after-exit-snippet-hook
+          (lambda ()
+            (whitespace-cleanup-region yas-snippet-beg yas-snippet-end)
+           )
+ )
