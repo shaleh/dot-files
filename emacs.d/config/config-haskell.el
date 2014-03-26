310b31ae9fcbcfa0e1d1d5249036e95999938334
Sean Perry <shaleh@speakeasy.net>
2014-03-26 09:46:38 -0700

    update haskell support to properly install a hook

--- emacs.d/config/config-haskell.el
+++ emacs.d/config/config-haskell.el
@@ -3,4 +3,26 @@
              (concat my-site-lisp (convert-standard-filename "haskell-mode/"))
  )
 
-(add-hook 'haskell-mode-hook 'my/common-programming-hook)
+(add-hook 'haskell-mode-hook 'my/haskell-hook)
+
+(defun my/haskell-hook ()
+    (turn-on-haskell-simple-indent)
+
+    ;; Indent the below lines on columns after the current column.
+    (define-key haskell-mode-map (kbd "C-<right>")
+       (lambda ()
+         (interactive)
+         (haskell-move-nested 1)
+        )
+     )
+
+    ;; Same as above but backwards.
+    (define-key haskell-mode-map (kbd "C-<left>")
+       (lambda ()
+         (interactive)
+         (haskell-move-nested -1)
+        )
+     )
+
+    (my/common-programming-hook)
+ )
