ad3ba11c6fc9859e3f6126607f042b3827418dc1
Sean Perry <sean_perry@symantec.com>
2014-08-18 10:38:21 -0700

    Remove N from the diff args to simplify output.

--- check-changes.sh
+++ check-changes.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-diff -pruNb --exclude '*~' --exclude '.git' --exclude '*.elc' --exclude 'state' ~/.emacs.d emacs.d
+diff -prub --exclude '*~' --exclude '.git' --exclude '*.elc' --exclude 'state' ~/.emacs.d emacs.d
 diff -ub ~/.emacs dot.emacs
 diff -ub ~/.ackrc ackrc
