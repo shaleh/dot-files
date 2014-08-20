85411fe1fd5c3027f17a102eac33a6f31e501e88
Sean Perry <shaleh@speakeasy.net>
2014-08-20 08:32:09 -0700

    simplify setup of common mode hook. Remove files which just setup the hook.

--- emacs.d/config/config-graphviz.el
+++ emacs.d/config/config-graphviz.el
@@ -1,3 +1,2 @@
 (autoload 'graphviz-dot-mode "graphviz-dot-mode")
 (add-to-list 'auto-mode-alist '("\\.dot$" . graphviz-dot-mode))
-(add-hook 'graphviz-dot-mode-hook 'my/common-programming-hook)
--- emacs.d/config/config-java.el
+++
@@ -1,1 +0,0 @@
-(add-hook 'java-mode-hook 'my/common-programming-hook)
