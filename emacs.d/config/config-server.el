23fc9a3fdd418116bd88bd82d36cd1bd37eaf879
Sean Perry <sean_perry@symantec.com>
2014-08-22 13:33:01 -0700

    Fix handling of C-x k.

--- emacs.d/config/config-server.el
+++ emacs.d/config/config-server.el
@@ -1,10 +1,16 @@
 (server-start)
 
+(defun my/kill-buffer-or-server-edit ()
+  (interactive)
+  (if server-buffer-clients
+      (server-edit)
+    (kill-buffer)
+   )
+ )
+
 ;; make C-x k work for emacsclient sessions too.
 (add-hook 'server-switch-hook
   (lambda ()
-    (when server-buffer-clients
-      (local-set-key (kbd "C-x k") 'server-edit)
-     )
+    (local-set-key (kbd "C-x k") 'my/kill-buffer-or-server-edit)
    )
  )
