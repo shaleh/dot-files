f39bf3a10d89e103cb447fdb4b273d194474c9bc
Sean Perry <sean_perry@symantec.com>
2014-10-13 11:01:12 -0700

    fix up formatting

--- emacs.d/config/config-c.el
+++ emacs.d/config/config-c.el
@@ -1,12 +1,15 @@
 (add-hook 'c-mode-common-hook
   (lambda()
-    (c-set-style 'bsd)
+    (c-set-style "bsd")
+    (setq c-basic-offset 4)
+    (c-set-offset 'substatement-open '0)
     (local-set-key (kbd "C-x C-o") 'ff-find-other-file)
    )
  )
 
 (add-hook 'c++-mode-hook
   (lambda()
-    (c-set-style 'stroustrup)
+    (c-set-style "stroustrup")
+    (local-set-key (kbd "C-x C-o") 'ff-find-other-file)
    )
  )
