089d8e0395a2354036bf30aa4b026effe245b7f4
Sean Perry <sean.perry@hp.com>
2015-10-22 12:04:07 -0700

    remove marmelade

--- emacs.d/config/config-packages.el
+++ emacs.d/config/config-packages.el
@@ -1,10 +1,9 @@
 (when (>= emacs-major-version 24)
   (require 'package)
   (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-			   ("marmalade" . "http://marmalade-repo.org/packages/")
-			   ("melpa" . "http://melpa.milkbox.net/packages/"))
+			   ("melpa" . "http://melpa.org/packages/")
+                           )
    )
-
   (package-initialize)
 
   (unless package-archive-contents
