907da50f3ae16e420fef2ef10382ed5969dfe30b
Sean 'Shaleh' Perry <shaleh@speakeasy.net>
2017-07-10 11:06:08 -0700

    Reorg to allow for easier checking of package states.

--- emacs.d/package-list.el
+++ emacs.d/package-list.el
@@ -1,43 +1,56 @@
 (require 'my-functions)
 
-; Get list from package-activated-list.
-(let ((my-package-list '(adoc-mode
-                         browse-kill-ring
-                         color-theme
-                         color-theme-approximate
-                         color-theme-buffer-local
-                         color-theme-solarized
-                         demangle-mode
-                         elpy
-                         fic-mode
-                         find-file-in-project
-                         flycheck
-                         flycheck-haskell
-                         flycheck-rust
-                         git-timemachine
-                         google-this
-                         graphviz-dot-mode
-                         haskell-mode
-                         hide-lines
-                         highlight-indentation
-                         indent-guide
-                         jinja2-mode
-                         js2-mode
-                         json-mode
-                         ;;kill-ring-ido
-                         lorem-ipsum
-                         markdown-mode
-                         mwim
-                         noflet
-                         python-mode
-                         pyvenv
-                         rebox2
-                         rust-mode
-                         thrift
-                         toml-mode
-                         whole-line-or-region
-                         xcscope
-                         yaml-mode
-                         yasnippet)))
-  (mapc #'my/require-package my-package-list)
+(setq my-package-list '(adoc-mode
+                        browse-kill-ring
+                        color-theme
+                        color-theme-approximate
+                        color-theme-buffer-local
+                        color-theme-solarized
+                        demangle-mode
+                        elpy
+                        fic-mode
+                        find-file-in-project
+                        flycheck
+                        flycheck-haskell
+                        flycheck-rust
+                        git
+                        git-timemachine
+                        go-mode
+                        google-this
+                        graphviz-dot-mode
+                        haskell-mode
+                        hide-lines
+                        highlight-indentation
+                        indent-guide
+                        jinja2-mode
+                        js2-mode
+                        json-mode
+                        json-reformat
+                        ;; kill-ring-ido
+                        lorem-ipsum
+                        lua-mode
+                        markdown-mode
+                        mwim
+                        noflet
+                        pass
+                        password-store
+                        python-mode
+                        pyvenv
+                        rebox2
+                        rust-mode
+                        thrift
+                        toml-mode
+                        whole-line-or-region
+                        xcscope
+                        yaml-mode
+                        yasnippet))
+
+(mapc #'my/require-package my-package-list)
+
+(defun my/package-inconsistency()
+  (-difference my-package-list (sort (delq nil (delete-dups package-activated-list)) #'string<))
+ )
+
+(defun my/package-installed-inconsistency()
+  (-difference (sort (delq nil (delete-dups package-activated-list)) #'string<) my-package-list )
  )
