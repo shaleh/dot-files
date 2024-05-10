3f20eb7db0640b1d22fc04f1967a3d68d61ca395
Sean 'Shaleh' Perry <shalehperry@gmail.com>
2024-05-10 00:42:43 -0700

    prompt for now

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,4 +1,11 @@
 function fish_prompt
     set -g __fish_git_prompt_showcolorhints true
-    printf "[%s@%s %s]\n%s \$ " (whoami) (hostname) (prompt_pwd) (fish_git_prompt)
+    set -l last_status $status
+    set -l stat
+
+    if test $last_status -ne 0
+        set stat (set_color red)" [$last_status]"(set_color normal)
+    end
+
+    printf "[%s %s]%s\n%s \$ " (prompt_login) (prompt_pwd --dir-length 3 --full-length-dirs 2) "$stat" (fish_git_prompt)
 end
