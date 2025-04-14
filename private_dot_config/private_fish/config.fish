e2934b3c320f1e171ec0531c78732fa80c831921
Sean Perry <shalehperry@gmail.com>
2025-04-13 22:29:39 -0700

    Add pay-respects

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,3 +1,9 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+    function fish_command_not_found --on-event fish_command_not_found
+        eval $(_PR_LAST_COMMAND="$argv" _PR_SHELL="fish" _PR_ALIAS="$(alias)" _PR_MODE="suggestion" "pay-respects")
+    end
+
+    function oops -d "Suggest fixes to the previous command"
+        eval $(_PR_LAST_COMMAND="$(history | head -n 1)" _PR_ALIAS="$(alias)" _PR_SHELL="fish" "pay-respects")
+    end
 end
