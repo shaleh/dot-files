53ac7f20140044bb835f6fb807f13ed83e09f89e
Sean Perry <shalehperry@gmail.com>
2025-11-20 14:29:16 -0800

    transition to brewfile instead of chezmoi package data.

---
+++ bootstrap-darwin.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+set -e
+
+brew bundle install --global
--- install.sh
+++ install.sh
@@ -1,55 +1,55 @@
 #!/bin/sh
 
-# FIXME inline install.sh here instead of using curl | sh
-# FIXME consider using packages to install chezmoi on deb and rpm-based systems
-
 set -e
 
 cd "${HOME}"
 
 is_command() {
-	type "${1}" >/dev/null 2>&1
+    type "${1}" >/dev/null 2>&1
 }
 
 if [ -n "${LOGNAME}" ]; then
-	username="${LOGNAME}"
+    username="${LOGNAME}"
 elif [ -n "${USER}" ]; then
-	username="${USER}"
+    username="${USER}"
 elif [ -n "${USERNAME}" ]; then
-	username="${USERNAME}"
+    username="${USERNAME}"
 elif is_command whoami; then
-	username="$(whoami)"
+    username="$(whoami)"
 elif is_command logname; then
-	username="$(logname)"
+    username="$(logname)"
 else
-	printf "unable to determine username" 1>&2
-	exit 1
-fi
-
-sudo=
-if [ "${username}" != "root" ]; then
-	sudo="sudo "
+    printf "unable to determine username" 1>&2
+    exit 1
 fi
 
 chezmoi=chezmoi
 if is_command chezmoi; then
-	chezmoi --version
+    chezmoi --version
 elif is_command "${HOME}/.local/bin/chezmoi"; then
-	chezmoi="${HOME}/.local/bin/chezmoi"
+    chezmoi="${HOME}/.local/bin/chezmoi"
 elif is_command "${HOME}/bin/chezmoi"; then
-	chezmoi="${HOME}/bin/chezmoi"
+    chezmoi="${HOME}/bin/chezmoi"
 elif is_command curl; then
-	sh -c "$(curl -fsSL get.chezmoi.io/lb)"
-	chezmoi="$HOME/.local/bin/chezmoi"
+    sh -c "$(curl -fsSL get.chezmoi.io/lb)"
+    chezmoi="$HOME/.local/bin/chezmoi"
 elif is_command wget; then
-	sh -c "$(wget -qO- get.chezmoi.io/lb)"
-	chezmoi="$HOME/.local/bin/chezmoi"
+    sh -c "$(wget -qO- get.chezmoi.io/lb)"
+    chezmoi="$HOME/.local/bin/chezmoi"
 else
-	echo "unable to install chezmoi" 1>&2
-	exit 1
+    echo "unable to install chezmoi" 1>&2
+    exit 1
 fi
 
 ${chezmoi} init --apply "shaleh/dot-files"
 
-shell="$(awk -F : "\$1 == \"${username}\" { print \$7 }" /etc/passwd)"
-exec "${shell}"
+cd "$(chezmoi source-path)"
+if [ "$(uname -o)" = "Darwin" ]; then
+	echo "Running bootstrap for Darwin"
+    ./bootstrap-darwin.sh
+elif [ -f "/etc/debian-version" ]; then
+	echo "Running bootstrap for Debian"
+	./bootstrap-debian.sh
+else
+	echo "No bootstrap to run"
+fi
