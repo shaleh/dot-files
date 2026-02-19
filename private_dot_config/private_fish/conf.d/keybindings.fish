9f533c8d09ff5b44aa7ddd2cd8507a5815dd6d1f
Sean Perry <shalehperry@gmail.com>
2026-02-19 11:41:29 -0800

    Update to current fish version.
    The old user key bindings function fish was not happy with.

--- private_dot_config/private_fish/conf.d/keybindings.fish
+++ private_dot_config/private_fish/conf.d/keybindings.fish
@@ -1,4 +1,10 @@
-function fish_user_key_bindings
-    bind ! bind_bang
-    bind '$' bind_dollar
-end
+# Custom function that does prefix search, the original search is still
+# available with `<C-p>`
+# Problem with these is that the first result is the same as autocomplete
+bind up up-or-prefix-search
+bind \e\[A up-or-prefix-search
+bind down down-or-prefix-search
+bind \e\[B down-or-prefix-search
+# Make !$ and !! work how my muscles remember them.
+bind ! bind_bang
+bind '$' bind_dollar
